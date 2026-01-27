e7e25e4123365aec72d85da4e40b5c6676d52ce3
minhs1mple <ducminhns2004@gmail.com>
2026-01-27 18:40:14 +0700

    feat: initial commit

---
+++ sep490-g94-sp26-be/src/main/java/vn/edu/fpt/Sep490G94Sp26BeApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep490G94Sp26BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sep490G94Sp26BeApplication.class, args);
+	}
+
+}
---
+++ sep490-g94-sp26-be/src/test/java/vn/edu/fpt/Sep490G94Sp26BeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep490G94Sp26BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
