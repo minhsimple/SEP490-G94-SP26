689cdd9cd5215c87bd258ca79ccf48118c5c17c4
Vinh <vinhnnhe180730@fpt.edu.vn>
2026-03-24 22:02:49 +0700

    dd

---
+++ sep490-g94-sp26-be/src/main/java/vn/edu/fpt/dto/response/payment/PaymentRequest.java
@@ -0,0 +1,4 @@
+package vn.edu.fpt.dto.response.payment;
+
+public class PaymentRequest {
+}
--- sep490-g94-sp26-be/src/main/java/vn/edu/fpt/exception/ERROR_CODE.java
+++ sep490-g94-sp26-be/src/main/java/vn/edu/fpt/exception/ERROR_CODE.java
@@ -131,7 +131,7 @@ public enum ERROR_CODE {
     PAYMENT_CONFIG_MISSING(15001, "Payment configuration is missing", HttpStatus.BAD_REQUEST),
     PAYMENT_PROVIDER_ERROR(15002, "Payment provider error", HttpStatus.BAD_GATEWAY),
     PAYMENT_INVALID_SIGNATURE(15003, "Payment webhook signature is invalid", HttpStatus.BAD_REQUEST),
-    PAYMENT_INVALID_STATE(15004, "Contract state is not valid for payment", HttpStatus.BAD_REQUEST),
+    PAYMENT_INVALID_STATE(15004, "Contract state is not valid for payos", HttpStatus.BAD_REQUEST),
 
     /**
      * Other Error
