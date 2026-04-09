cfa3fd6374d231fe08f85b31c935019fd4b1237a
Bui Khanh Toan <144225935+KhanhToan13@users.noreply.github.com>
2026-04-10 02:59:39 +0700

    seat layout update

--- sep490-g94-sp26-fe/src/app/pages/seat-layout/Seat-layout.ts
+++ sep490-g94-sp26-fe/src/app/pages/seat-layout/Seat-layout.ts
@@ -762,6 +762,9 @@ export class SeatLayoutComponent implements OnInit {
             zone.groups = [];
         }
 
+
+
+        
         let colorIndex = 0;
         for (const item of request.tableLayoutDetailRequestList ?? []) {
             const enumKey = String(item.tableLayoutEnum ?? '').toUpperCase();
