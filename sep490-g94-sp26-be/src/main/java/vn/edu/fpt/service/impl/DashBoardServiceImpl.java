77a0d3c738b38ad14bc55831c05d940a8df5f36c
Vinh <vinhnnhe180730@fpt.edu.vn>
2026-05-02 22:16:10 +0700

    fix dashboard

--- sep490-g94-sp26-be/src/main/java/vn/edu/fpt/service/impl/DashBoardServiceImpl.java
+++ sep490-g94-sp26-be/src/main/java/vn/edu/fpt/service/impl/DashBoardServiceImpl.java
@@ -503,15 +503,6 @@ public class DashBoardServiceImpl implements DashBoardService {
                 .map(i -> i.getTotalAmount() != null ? i.getTotalAmount() : BigDecimal.ZERO)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 
-        // Tổng tiền đã thu (payments thành công)
-        BigDecimal totalCollectedAmount = payments.stream()
-                .filter(p -> p.getPaymentState() == PaymentState.SUCCESS)
-                .map(Payment::getAmount)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-
-
-        // Tổng nợ còn lại
-        BigDecimal totalOutstandingDebt = totalExpectedRevenue.subtract(totalCollectedAmount);
 
         // Tổng tiền đã hoàn (refunded payments)
         BigDecimal totalRefundedAmount = payments.stream()
@@ -519,9 +510,20 @@ public class DashBoardServiceImpl implements DashBoardService {
                 .map(Payment::getAmount)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 
+        // Tổng tiền đã thu (payments thành công)
+        BigDecimal totalCollectedAmount = payments.stream()
+                .filter(p -> p.getPaymentState() == PaymentState.SUCCESS)
+                .map(Payment::getAmount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add).subtract(totalRefundedAmount);
+
+
+        // Tổng nợ còn lại
+        BigDecimal totalOutstandingDebt = totalExpectedRevenue.subtract(totalCollectedAmount);
+
+
         return AccountantDashBoardResponse.CashFlow.builder()
                 .totalExpectedRevenue(totalExpectedRevenue)
-                .totalCollectedAmount(totalCollectedAmount.subtract(totalRefundedAmount))
+                .totalCollectedAmount(totalCollectedAmount)
                 .totalOutstandingDebt(totalOutstandingDebt)
                 .totalRefundedAmount(totalRefundedAmount)
                 .build();
